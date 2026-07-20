67117d6c1f559066a21279280d535b957ce2aee5
RohanPatil01-dev <rohanpatil96273@gmail.com>
2026-07-20 11:53:29 +0530

    updated code

--- evenodd.java
+++ evenodd.java
@@ -6,12 +6,22 @@ public class evenodd {
     static void main() {
         Scanner sc=new Scanner(System.in);
         System.out.println("enter no.");
+        System.out.println("enter size of array");
+        int s=sc.nextInt();
+        int arr[]=new int[s];
+        System.out.println("enter  values");
+        for(int i=0;i<arr.length;i++){
+            arr[i]=i;
+
+        }
+
         int a=sc.nextInt();
-        if(a%2==0){
+        for(int i=0;i<arr.length;i++){
+        if(i%2==0){
             System.out.println("even");
         }
         else{
             System.out.println("odd");
-        }
+        }}
     }
 }
